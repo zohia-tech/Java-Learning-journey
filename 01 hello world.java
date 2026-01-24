16727150076102eb1706b5f72974ee17898333f7
zohia <zohia758@gmail.com>
2026-01-24 22:26:00 +0500

    Add Hello World program in Java

---
+++ 01 hello world.java
@@ -0,0 +1,7 @@
+public class Main { // Defines the class named "Main"
+
+    public static void main(String[] args) { // Main method – entry point of the program
+
+        System.out.println("Hello, world!"); // Prints "Hello, world!" to the console
+    }
+}
