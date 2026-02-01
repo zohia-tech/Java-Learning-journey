585b18b1ee153b4ef43b7e4d136c0cd42c3c5632
zohia <zohia758@gmail.com>
2026-02-01 18:46:48 +0500

    Create 04.CWHP_05_TakingInput.java

---
+++ 04.CWHP_05_TakingInput.java
@@ -0,0 +1,6 @@
+public class CWHP_05_TakingInput { // Class for demonstrating taking input (basic structure)
+    public static void main(String[] args) { // Main method (entry point of the program)
+
+        System.out.println("It works!"); // Prints a message to confirm the program runs correctly
+    }
+}
