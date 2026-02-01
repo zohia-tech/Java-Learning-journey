609ad51c3393f4b340e127ec52c8e84bef3ac02e
zohia <zohia758@gmail.com>
2026-02-01 18:43:02 +0500

    Add Hello World program with addition functionality

---
+++ 02.Hello World program with addition.java
@@ -0,0 +1,14 @@
+public class Main { // Main class where program starts
+    public static void main(String[] args) { // Main method (entry point of program)
+
+        System.out.println("Hello, World!"); // Prints Hello, World! on the screen
+
+        int num1 = 6; // Stores first number
+        int num2 = 5; // Stores second number
+        int num3 = 7; // Stores third number
+
+        int sum = num1 + num2 + num3; // Adds all three numbers and stores the result in sum
+
+        System.out.println(sum); // Prints the final sum
+    }
+}
