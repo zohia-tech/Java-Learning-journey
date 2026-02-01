b567cdcb4e19af53976dd3179aac07b19559f1c2
zohia <zohia758@gmail.com>
2026-02-01 18:44:38 +0500

    Add Java program to demonstrate literals
    Demonstrate different types of literals in Java.

---
+++ 03.Literals program.java
@@ -0,0 +1,11 @@
+public class CHW_04_Literals { // Class to demonstrate different types of literals
+    public static void main(String[] args) { // Main method (program execution starts here)
+
+        byte age = 34; // Stores a byte literal value
+        char ch = 'A'; // Stores a character literal
+        float f1 = 5.6f; // Stores a float literal (f is required for float)
+        double d1 = 4.66; // Stores a double literal
+
+        System.out.println(age); // Prints the value of age
+    }
+}
