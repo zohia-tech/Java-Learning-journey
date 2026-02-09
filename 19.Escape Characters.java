9d5428cfa7ca6077552644d772b744ac9839bb74
zohia <zohia758@gmail.com>
2026-02-09 19:44:03 +0500

    Create 19.Escape Characters.java

---
+++ 19.Escape Characters.java
@@ -0,0 +1,8 @@
+public class string { // Class name
+    public static void main(String[] args) { // Main method starts here
+        
+        System.out.println("He said, \"I believe that the Earth is Flat\"."); // Print sentence with double quotes inside
+        
+        System.out.println("she said, \'But the Earth is spherical\'"); // Print sentence with single quotes inside
+    }
+}
