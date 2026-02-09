6d20f21893ba4082d7153c4c79fade3f742794db
zohia <zohia758@gmail.com>
2026-02-09 19:26:33 +0500

    Create 15. do while loops.java

---
+++ 15. do while loops.java
@@ -0,0 +1,11 @@
+public class WhileLoop { // Class name
+    public static void main(String[] args) { // Main method starts here
+        
+        int i = 10; // Initialize variable with value 10
+        
+        do { 
+            System.out.println(i); // Print the value of i
+            i--; // Decrease i by 1
+        } while (i > 10); // Condition is checked after executing the loop once
+    }
+}
