8ecafe2b331940e41209cd0e02ff5257428296c9
zohia <zohia758@gmail.com>
2026-02-09 19:25:08 +0500

    Create 13.whileloop.java

---
+++ 13.whileloop.java
@@ -0,0 +1,11 @@
+public class WhileLoop { // Class name
+    public static void main(String[] args) { // Main method starts here
+        
+        int i = 10; // Initialize variable with value 10
+        
+        while (i > 0) { // Loop runs while i is greater than 0
+            System.out.println(i); // Print the value of i
+            i--; // Decrease i by 1 each time
+        }
+    }
+}
