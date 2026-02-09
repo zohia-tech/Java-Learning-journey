8040013543bd621801c791d357a671a83b5a604a
zohia <zohia758@gmail.com>
2026-02-09 19:44:56 +0500

    Add StringExample class demonstrating indexOf method

---
+++ 20.String Methods.java
@@ -0,0 +1,7 @@
+public class StringExample {
+    public static void main(String[] args) {
+        String quote = "To be or not to be";
+        System.out.println(quote.indexOf("be"));    // index of text
+        System.out.println(quote.indexOf("r"));     // index of character
+    }
+}
