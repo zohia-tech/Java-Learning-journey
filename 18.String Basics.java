debc0560fbd24a1ab3282f625217e0a30be9ecf8
zohia <zohia758@gmail.com>
2026-02-09 19:42:48 +0500

    Create 18.String Basics.java

---
+++ 18.String Basics.java
@@ -0,0 +1,9 @@
+public class StringExample { // Class name
+    public static void main(String[] args) { // Main method starts here
+        
+        String name; // Declare a string variable
+        name = "Diablo"; // Assign value to the variable
+        
+        System.out.println("My name is " + name); // Print message with the variable
+    }
+}
