90e5b5151922bf9983753f71b9082664605515c8
zohia <zohia758@gmail.com>
2026-03-03 21:43:46 +0500

    Add Java program demonstrating compiled and interpreted

---
+++ Interpreted vs Compiled Languages.java
@@ -0,0 +1,12 @@
+// Java program explaining that Java is both compiled and interpreted
+class CompiledVsInterpretedDemo {
+
+    public static void main(String[] args) {
+
+        // Java source code (.java) is compiled into bytecode (.class) by the compiler
+        System.out.println("Java source code is first compiled to bytecode.");
+
+        // JVM then interprets or executes the bytecode on the running system
+        System.out.println("Then JVM interprets/runs that bytecode.");
+    }
+}
