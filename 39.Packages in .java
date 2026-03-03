086cc2100fcbbfc4cf09b629c50dea5ee6adf722
zohia <zohia758@gmail.com>
2026-03-03 21:52:47 +0500

    Create 39.Packages in .java

---
+++ 39.Packages in .java
@@ -0,0 +1,11 @@
+// Declaring a package named 'mypackage'
+package mypackage;
+
+public class PackageExample {
+
+    public static void main(String[] args) {
+
+        // Print message to show this class belongs to a package
+        System.out.println("This class belongs to mypackage package.");
+    }
+}
